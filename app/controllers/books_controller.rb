bff9c9e26646bbd8c1d5822c5449769bf3475ebc
EC2 Default User <ec2-user@ip-172-31-8-200.ap-northeast-1.compute.internal>
2023-02-23 00:20:36 +0000

    コントローラ完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,20 +3,37 @@ class BooksController < ApplicationController
   end
 
   def edit
+     @book = Book.find(params[:id])
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
+
+    #book = Book.new(book_params)
+    #book.save
+    #redirect_to '/books'#
+
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to "/books"
+    else
+     @books = Book.all
+     render:index
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to '/books'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def index
+    @books = Book.all
     @book = Book.new
-    @books = Book.all  
   end
 
    private
